fb85f24e88022017f086669d75e09e392acf071a
Matthew Ballance <matt.ballance@gmail.com>
2019-03-30 15:59:54 -0400

    Add basic unit test structure

---
+++ ve/unit/src/ccrt_unit_main.cpp
@@ -0,0 +1,16 @@
+/*
+ * ccrt_unit_main.cpp
+ *
+ *  Created on: Mar 30, 2019
+ *      Author: ballance
+ */
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+
+	::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
+
+
